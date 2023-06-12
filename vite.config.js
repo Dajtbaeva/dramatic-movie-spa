7275957ae21c8e407d22bedad18a672f209396c1
Darina <dajtbaeva@gmail.com>
2023-06-12 12:35:02 +0600

    fix

--- vite.config.js
+++ vite.config.js
@@ -7,11 +7,11 @@ import vuetify from "vite-plugin-vuetify";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vuetify({ autoImport: true })],
-  build: {
-    rollupOptions: {
-      external: ["axios"],
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     external: ["axios"],
+  //   },
+  // },
 //   resolve: {
 //     alias: {
 //       axios: "axios/dist/axios.min.js",
